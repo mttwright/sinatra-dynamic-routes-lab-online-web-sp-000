e0387a9ea7f8bef213b865126c16d08753a2a186
Matt Wright <mttwright@gmail.com>
2020-10-31 21:26:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,25 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  
+  get '/reversename/:name' do
+    "Your name is #{params[:name].reverse}"
+  end
+  
+  get '/square/:number' do
+    "The number is #{params[:number].to_i * params[:number].to_i}"
+  end
+  
+  get '/say/:number/:phrase' do
+    "#{params[:phrase] * params[:number].to_i}"
+  end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1].concat(" ",params[:word2]," ",params[:word3]," ",params[:word4]," ",params[:word5])}"
+  end
+  
+  get '/:operation/:num1/:num2' do
+    erb :operation
+  end
 
 end
