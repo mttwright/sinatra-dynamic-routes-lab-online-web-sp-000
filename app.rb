be4b228363ad0ba2c56939b967fe28c1d874dcc3
Matt Wright <mttwright@gmail.com>
2020-10-31 22:27:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    "#{params[:word1].concat(" ",params[:word2]," ",params[:word3]," ",params[:word4]," ",params[:word5])}"
+    "#{params[:word1].concat(" ",params[:word2]," ",params[:word3]," ",params[:word4]," ",params[:word5],".")}"
   end
   
   get '/:operation/:num1/:num2' do
